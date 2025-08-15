f7f63665ea24b7db62b7ab60608d9c2d88267668
Matt Tonks <>
2025-08-15 17:35:02 +0100

    Channel refactor
    Moves the bulk of the logic into a re-usable function, yields the string when ready and then loops over the lines and echos them out like before

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
+function getLinesChannel($file)
+{
+    $string = "";
+
+    // read the file in 8 byte chunks and continue until no more
+    while (($chunk = fread($file, 8)) !== false && $chunk !== '') {
+        // check if the chunk contains a newline character
+        $position = strpos($chunk, "\n");
+
+        if($position !== false) {
+            // get everything up until the new line and append it to the running log 
+            $string .= substr($chunk, 0, $position);
+
+            yield $string;
+
+            // reset to start keeping track until next newline
+            $string = "";
+
+            // Keep only the part after the newline for next iteration
+            $chunk = substr($chunk, $position + 1);
+        }
+
+        $string .= $chunk;
+    }
+}
+
 // rb opens the file in read binary mode
 $file = fopen('./messages.txt', 'rb');
 
@@ -7,29 +33,8 @@ if (!$file) {
     die("Unable to open file");
 }
 
-// for keeping track of what has been read so far
-$string = "";
-
-// read the file in 8 byte chunks and continue until no more
-while (($chunk = fread($file, 8)) !== false && $chunk !== '') {
-    // check if the chunk contains a newline character
-    $position = strpos($chunk, "\n");
-
-    if($position !== false) {
-        // get everything up until the new line and append it to the running log 
-        $string .= substr($chunk, 0, $position);
-
-        // log the complete line
-        echo 'read: '. $string . PHP_EOL;
-
-        // reset to start keeping track until next newline
-        $string = "";
-
-        // Keep only the part after the newline for next iteration
-        $chunk = substr($chunk, $position + 1);
-    }
-
-    $string .= $chunk;
+foreach(getLinesChannel($file) as $line) {
+    echo 'read: '. $line . PHP_EOL;
 }
 
 // tidy up
