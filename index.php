88da9a87fede29d49a35a6a24174ebdb443f0102
Matt Tonks <>
2025-08-15 17:22:33 +0100

    Newlines
    learned how to read in 8 bytes but still preserve readability of text in a file.

--- index.php
+++ index.php
@@ -7,15 +7,29 @@ if (!$file) {
     die("Unable to open file");
 }
 
-// while we aren't at the end of the file, read 8 bytes and echo
-while (!feof($file)) {
-    $chunk = fread($file, 8);
+// for keeping track of what has been read so far
+$string = "";
 
-    if ($chunk === false) {
-        break;
+// read the file in 8 byte chunks and continue until no more
+while (($chunk = fread($file, 8)) !== false && $chunk !== '') {
+    // check if the chunk contains a newline character
+    $position = strpos($chunk, "\n");
+
+    if($position !== false) {
+        // get everything up until the new line and append it to the running log 
+        $string .= substr($chunk, 0, $position);
+
+        // log the complete line
+        echo 'read: '. $string . PHP_EOL;
+
+        // reset to start keeping track until next newline
+        $string = "";
+
+        // Keep only the part after the newline for next iteration
+        $chunk = substr($chunk, $position + 1);
     }
 
-   echo 'read:'.$chunk . PHP_EOL;
+    $string .= $chunk;
 }
 
 // tidy up
